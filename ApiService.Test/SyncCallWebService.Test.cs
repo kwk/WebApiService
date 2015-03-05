b70b8955f860307fa2d76b259499ce9e8e1c8024
Christian <csc@sondev.de>
2015-03-05 13:52:21 +0100

    second shot

---
+++ ApiService.Test/SyncCallWebService.Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//
+using ApiService;
+
+namespace ApiService.Test
+{
+    [TestClass]
+    public class SyncCallWebService : WebApiService
+    {
+        public SyncCallWebService()
+        {
+            this.ServiceUri = "http://wsf.cdyne.com/WeatherWS/Weather.asmx/";
+        }
+
+        [TestMethod]
+        public void TestShouldReturnWeatherForecastForNewYork()
+        {
+            var parameters = new List<ApiServiceParameter>();
+            const string methodName = "GetCityForecastByZIP";
+
+            parameters.Add(new ApiServiceParameter { ParamName = "ZIP", ParamValue = "10001" });
+
+            var res = CallWebApiSync(methodName, parameters);
+
+            if (res != null)
+            {
+                var apiResult = res.ReadAsStringAsync().Result;
+
+                bool containsNewYork = apiResult.Contains("New York");
+
+                Assert.IsNotNull(apiResult);
+                Assert.AreEqual(true,containsNewYork);
+            }
+
+        }
+
+
+    }
+}
---
+++ ApiService/ApiService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// via Nuget
+using System.Net.Http; // Microsoft HTTP Client Libraries
+
+namespace ApiService
+{
+    public class ApiServiceParameter
+    {
+        public string ParamName { get; set; }
+        public string ParamValue { get; set; }
+
+        /// <summary>
+        /// ParamName + ParamValue = query
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return ParamName + "=" + ParamValue;
+        }
+    }
+
+    public class WebApiService
+    {
+        /// <summary>
+        /// Uri of the webservice
+        /// </summary>
+        public string ServiceUri { get; set; }
+
+        /// <summary>
+        /// Performs a synchronous call to the passed method name. 
+        /// </summary>
+        /// <param name="methodName">Method to call</param>
+        /// <param name="parameters">List of the query parameters</param>
+        /// <returns>HttpContent</returns>
+        public HttpContent CallWebApiSync(string methodName, List<ApiServiceParameter> parameters = null)
+        {
+            if (string.IsNullOrWhiteSpace(ServiceUri)) return null;
+
+            string uri = ServiceUri + methodName;
+
+            if (parameters != null && parameters.Count > 0)
+            {
+                uri += "?";
+
+                foreach (var apiServiceParameter in parameters)
+                {
+                    uri += apiServiceParameter.ToString() + "&";
+                }
+
+                if (uri[uri.Length - 1] == '&')
+                {
+                    uri = uri.Substring(0, uri.Length - 1);
+                }
+            }
+
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(uri).Result;
+                return response.Content;
+            }
+        }
+    }
+
+
+}

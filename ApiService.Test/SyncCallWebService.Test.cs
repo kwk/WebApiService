15b31561686b3d887275df0c462afbc2693dcbe7
Christian <csc@sondev.de>
2015-03-06 16:18:35 +0100

    Tried to add Proxy Server support

--- ApiService.Test/SyncCallWebService.Test.cs
+++ ApiService.Test/SyncCallWebService.Test.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -31,7 +33,7 @@ namespace ApiService.Test
             if (res != null)
             {
                 var apiResult = res.ReadAsStringAsync().Result;
-
+                
                 bool containsNewYork = apiResult.Contains("New York");
 
                 Assert.IsNotNull(apiResult);
@@ -40,6 +42,38 @@ namespace ApiService.Test
 
         }
 
+        [TestMethod]
+        public void TestShouldReturnWeatherForecastForNewYorkWithProxyServer()
+        {
+            var cookies = new CookieContainer();
+
+            var httpClientHandler = new HttpClientHandler
+            {
+                CookieContainer = cookies,
+                UseCookies = true,
+                UseDefaultCredentials = true,
+                Proxy = new WebProxy("http://192.168.0.254:8080", false, new string[] { }),
+                UseProxy = true
+            };
+
+            var parameters = new List<ApiServiceParameter>();
+            const string methodName = "GetCityForecastByZIP";
+
+            parameters.Add(new ApiServiceParameter { ParamName = "ZIP", ParamValue = "10001" });
+
+            var res = CallWebApiSync(methodName, httpClientHandler, parameters);
+
+            if (res != null)
+            {
+                var apiResult = res.ReadAsStringAsync().Result;
+                
+                bool containsNewYork = apiResult.Contains("New York");
+
+                Assert.IsNotNull(apiResult);
+                Assert.AreEqual(true,containsNewYork);
+            }
+
+        }
 
     }
 }
--- ApiService/ApiService.cs
+++ ApiService/ApiService.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 // via Nuget
 using System.Net.Http; // Microsoft HTTP Client Libraries
 
+
 namespace ApiService
 {
+    /// TODO: Implement usage of a Proxy-Server 
+    
     public class ApiServiceParameter
     {
         public string ParamName { get; set; }
@@ -41,7 +44,7 @@ namespace ApiService
         {
             if (string.IsNullOrWhiteSpace(ServiceUri)) return null;
 
-            string uri = ServiceUri + methodName;
+            string uri = ServiceUri + methodName; 
 
             if (parameters != null && parameters.Count > 0)
             {
@@ -59,6 +62,40 @@ namespace ApiService
             }
 
             using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(uri).Result;
+                return response.Content;
+            }
+        }
+
+        /// <summary>
+        /// Performs a synchronous call to the passed method name. 
+        /// </summary>
+        /// <param name="methodName">Method to call</param>
+        /// <param name="parameters">List of the query parameters</param>
+        /// <returns>HttpContent</returns>
+        public HttpContent CallWebApiSync(string methodName, HttpClientHandler handler, List<ApiServiceParameter> parameters = null)
+        {
+            if (string.IsNullOrWhiteSpace(ServiceUri)) return null;
+
+            string uri = ServiceUri + methodName;
+
+            if (parameters != null && parameters.Count > 0)
+            {
+                uri += "?";
+
+                foreach (var apiServiceParameter in parameters)
+                {
+                    uri += apiServiceParameter.ToString() + "&";
+                }
+
+                if (uri[uri.Length - 1] == '&')
+                {
+                    uri = uri.Substring(0, uri.Length - 1);
+                }
+            }
+
+            using (var client = new HttpClient(handler))
             {
                 var response = client.GetAsync(uri).Result;
                 return response.Content;
